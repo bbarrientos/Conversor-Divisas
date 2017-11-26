c38597342a46d57a9c4b85ec70e0baa7b83843f2
bbarrientos <bastianb471@gmail.com>
2017-11-26 17:42:44 -0300

    c

--- src/main/java/app/Lanzador.java
+++ src/main/java/app/Lanzador.java
@@ -4,9 +4,11 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 import vista.Ventana;
 
 public class Lanzador {
@@ -21,6 +23,9 @@ public class Lanzador {
             BufferedReader br = new BufferedReader(new FileReader(rutaArchivoDivisas));
             JsonElement jsonElement = jsonParser.parse(br);
             JsonObject object = jsonElement.getAsJsonObject();
+            List<> objs = object.fromJson(jsonElement, new TypeToken<List<Menu>>() {
+            }.getType());
+            object.fo
         }catch(IOException e){
             e.printStackTrace();
         }
